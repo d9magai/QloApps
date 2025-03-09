cc6f12bb30856ad1f8d72fc9192b95dc05ee96d9
d9magai <d9magai@gmail.com>
2025-03-09 14:46:18 +0900

    add Japanese translation settings file

---
+++ mails/ja/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Welcome!'] = 'いらっしゃいませ！';
+$_LANGMAIL['Process the payment of your order'] = 'ご注文の支払い手続き';
+$_LANGMAIL['Message from contact form'] = 'お問い合わせフォームからのメッセージ';
+$_LANGMAIL['New voucher for your credit slip #%s'] = '';
+$_LANGMAIL['The virtual product that you bought is available for download'] = '購入した仮想製品はダウンロード可能です';
+$_LANGMAIL['Your new password'] = '新しいパスワード';
+$_LANGMAIL['Fwd: Customer message'] = '転送: 顧客メッセージ';
+$_LANGMAIL['Your guest account has been transformed into a customer account'] = 'ゲストアカウントが顧客アカウントに変更されました';
+$_LANGMAIL['Package in transit'] = '配送中の荷物';
+$_LANGMAIL['Log: You have a new alert from your website'] = 'ログ: ウェブサイトから新しいアラートが届きました';
+$_LANGMAIL['Message from a customer'] = 'お客様からのメッセージ';
+$_LANGMAIL['New message regarding your order'] = 'ご注文に関する新しいメッセージ';
+$_LANGMAIL['Your order return status has changed'] = 'ご注文の返品ステータスが変更されました';
+$_LANGMAIL['Password query confirmation'] = 'パスワードクエリの確認';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'あなたのメッセージへの回答が利用可能です #ct%1$s #tc%2$s';
+$_LANGMAIL['New voucher for your order %s'] = 'ご注文 %s の新しいバウチャー';
+$_LANGMAIL['Newsletter confirmation'] = 'ニュースレターの確認';
+$_LANGMAIL['Email verification'] = 'メール認証';
+$_LANGMAIL['Newsletter voucher'] = 'ニュースレターバウチャー';
+$_LANGMAIL['Your review has been approved!'] = '';
+$_LANGMAIL['Please review us!'] = '';
+
+?>
